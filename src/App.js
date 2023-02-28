141217b9d3f60a6cb94a97c5d32d7d7dc6bdacb5
krlslman <selmankorall@gmail.com>
2023-02-28 18:52:30 +0300

    main and cards are rendered - styling icluding mixins are added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.scss';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
     <div className='App'>
       < Navbar />
-      <h1>Welcome to React</h1>
-      <p>Text lorem</p>
+      < Header />
+      < Main />
     </div>
   )
 }
